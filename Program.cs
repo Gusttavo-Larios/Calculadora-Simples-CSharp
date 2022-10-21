6b8dc1c2dd6d2e19679baa6d6a4b22af93d2de14
Gusttavo-Larios <eg9653641@gmail.com>
2022-10-20 23:05:30 -0300

    Operacoes matematicas em C Sharp

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using aritmetica.Models;
+
+Calculadora calculadora = new Calculadora();
+
+calculadora.Somar(10, 4);
+calculadora.Subtrair(10, 4);
+calculadora.Multiplicar(10, 4);
+calculadora.Dividir(10, 4);
+calculadora.Potencia(10, 4);
